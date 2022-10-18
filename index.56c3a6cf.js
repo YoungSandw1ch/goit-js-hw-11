4058e22d4260ac0779c224fe5f686aa61ad3f068
YoungSandw1ch <sersus1991@gmail.com>
2022-10-18 19:12:56 +0000

    Deploying to gh-pages from @ YoungSandw1ch/goit-js-hw-11@64e8ab1839e2b860029933fdbfbf3d32a3f3356d 🚀

---
+++ index.56c3a6cf.js
@@ -0,0 +1,2 @@
+const e={submitBtn:document.querySelector(".js-submit"),header:document.querySelector(".header")};e.submitBtn.addEventListener("click",(function(t){t.preventDefault(),e.header.classList.add("wrap")}));
+//# sourceMappingURL=index.56c3a6cf.js.map
---
+++ index.cfb7a07c.js
@@ -0,0 +1,2 @@
+!function(){var e={submitBtn:document.querySelector(".js-submit"),header:document.querySelector(".header")};e.submitBtn.addEventListener("click",(function(t){t.preventDefault(),e.header.classList.add("wrap")}))}();
+//# sourceMappingURL=index.cfb7a07c.js.map
