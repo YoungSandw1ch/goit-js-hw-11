f9715d06665260c57aae91c9b53709205d6254d7
Sergei Mikhailenko <sersus1991@gmail.com>
2022-10-23 23:38:43 +0300

    work with scroll

--- src/index.js
+++ src/index.js
@@ -1,25 +1,36 @@
 import './sass/index.scss';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import InfiniteScroll from 'infinite-scroll';
+import throttle from 'lodash.throttle';
 import getRefs from './js/get-refs';
 import fetchImages from './js/fetch-images';
 import { notifySuccess, notifyFailure } from './js/notify';
 import photoCardTpl from './templates/photo-card.hbs';
 
 const refs = getRefs();
+const THROTTLE_TIME = 250;
+let page = 1;
+let query = '';
 
 refs.form.addEventListener('submit', onSubmit);
 
 async function onSubmit(e) {
   e.preventDefault();
-  let page = 1;
-  const query = e.currentTarget.elements.searchQuery.value;
+  window.removeEventListener(
+    'scroll',
+    throttle(onScrollGallery, THROTTLE_TIME)
+  );
+  clearGallery();
+
+  query = e.currentTarget.elements.searchQuery.value;
   if (!query.trim()) return;
 
+  page = 1;
   const data = await fetchImages(query, page);
   const totalHits = data.totalHits;
   const photos = data.hits;
+
+  console.log(data);
 
   if (!photos.length) {
     notifyFailure();
@@ -27,30 +38,61 @@ async function onSubmit(e) {
   }
 
   wrapHeader();
+  clearGallery();
   renderPhotoCards(photos);
   notifySuccess(totalHits);
   simplelightbox();
-  infiniteScroll();
+
+  window.addEventListener('scroll', throttle(onScrollGallery, THROTTLE_TIME));
+}
+
+function onScrollGallery(query, page) {
+  const bodyRect = document.body.getBoundingClientRect();
+  const documentHeight = document.documentElement.clientHeight;
+
+  if (bodyRect.bottom < documentHeight + 200) {
+    console.log('done');
+    page += 1;
+    fetchImages(query, page).then(data => {
+      renderPhotoCards(data.hits);
+      console.log(data.hits);
+      return data;
+    });
+  }
+}
+
+async function searchImage(query, page) {
+  const data = await fetchImages(query, page);
+  const photos = data.hits;
+  return photos;
 }
 
 function renderPhotoCards(photos) {
   const photosCardMarkup = photoCardTpl(photos);
-  refs.gallery.innerHTML = photosCardMarkup;
+  refs.gallery.insertAdjacentHTML('beforeend', photosCardMarkup);
 }
 
 function simplelightbox() {
   let gallery = new SimpleLightbox('.gallery a');
 }
 
-function infiniteScroll() {
-  const options = {
-    path: '.pagination__next',
-    append: '.photo-link',
-    history: false,
-  };
-
-  let infScroll = new InfiniteScroll(refs.gallery, options);
+function clearGallery() {
+  refs.gallery.innerHTML = '';
 }
+
+// function infiniteScroll() {
+//   const options = {
+//     path: () => {
+//       page += 1;
+//       return
+//     },
+//     append: '.photo-link',
+//     history: false,
+//   };
+
+//   let infScroll = new InfiniteScroll(refs.gallery, options);
+// }
+
 //======================header animation================
 function wrapHeader() {
   refs.header.classList.add('wrap');
--- src/js/fetch-images.js
+++ src/js/fetch-images.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const BASE_URL = 'https://pixabay.com/api';
 const KEY = '25718154-32288cdf6837b8e29b2d2c309';
 const SEARCH_DETAILS =
-  '&image_type=photo&orientation=horizontal&safesearch=true&per_page=40';
+  '&image_type=photo&orientation=horizontal&safesearch=true&per_page=12';
 
 export default async function fetchImages(query, page) {
   const URL = `${BASE_URL}/?key=${KEY}&q=${query}${SEARCH_DETAILS}&page=${page}`;
